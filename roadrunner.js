63b98dc2a8ef1a16568df13ac19470a613aadeb3
Marshall <mad.mod.max@gmail.com>
2013-08-08 22:24:50 -0400

    Added search bar
    Converted to Bootstrap 3 (my old CSS pages weren't updated. It explained
    a lot, to be honest.)

--- roadrunner.js
+++ roadrunner.js
@@ -34,6 +34,16 @@ $(document).ready(function() {
 			$(this).parent().addClass('success');
 		}
 	});
+	
+	var rows = $('.table tr');
+	$('#search').keyup(function() {
+		var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+		
+		rows.show().filter(function() {
+			var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+			return !~text.indexOf(val);
+		}).hide();
+	});
 });
 
 /*function loadData() {
