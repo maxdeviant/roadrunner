aafbd2899790c4434bc9bf47c6b6c13469aad265
Marshall <mad.mod.max@gmail.com>
2013-08-28 18:50:23 -0400

    Fixed status code retrieval.
    Site appears to be working properly.

--- status.php
+++ status.php
@@ -5,12 +5,15 @@
 	$userlist = json_decode(fread($file, filesize($filename)), true);
 	fclose($file);
 
+	$count = 0;
 	foreach ($userlist as $key => $val) {
 		$script = 'wget --spider -S ' . $val['url'] . ' 2>&1 | grep "HTTP/" | awk \'{print $2}\'';
 
 		exec($script, $output);
 
-		$status[] = array('user' => $val['user'], 'status' => $output[0]);
+		$status[] = array('user' => $val['user'], 'status' => $output[$count]);
+		
+		$count++;
 	}
 	unset($val);
 
